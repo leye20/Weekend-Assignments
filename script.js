dbd81771fd2aad3917c46f3f8974fff2493772cf
leye20 <leyeagbo@gmail.com>
2020-03-29 21:07:11 -0600

    calculator function for multiplication works

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert('its me again');
+/*
+* Create a function that takes two numbers and a mathematical operator + - / * and will perform a calculation with the given numbers.
+*/
+
+function calculator (num1, operator, num2) {
+    if ( operator == '*' ); // Assign the operator
+    return num1 * num2;
+}
+
+console.log(calculator(3, '*', 6));
