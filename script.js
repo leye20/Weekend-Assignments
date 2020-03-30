c93eb2b682708ce62046085a6f93f3dfe7737919
leye20 <leyeagbo@gmail.com>
2020-03-29 21:17:06 -0600

    completed operation for multiplication and division

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 /*
+* Assignment 1
 * Create a function that takes two numbers and a mathematical operator + - / * and will perform a calculation with the given numbers.
 */
 
@@ -6,5 +7,11 @@ function calculator (num1, operator, num2) {
     if ( operator == '*' ); // Assign the operator
     return num1 * num2;
 }
-
+// Replicating for division
+function mathOperation (num1, operator, num2) {
+    if ( operator == '/' ); // Assign the operator
+    return num1 / num2;
+}
 console.log(calculator(3, '*', 6));
+console.log(mathOperation(6, '/', 13));
+
