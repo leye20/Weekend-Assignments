b17d71bf3cdb38e4d824592a5626aec370af8272
leye20 <leyeagbo@gmail.com>
2020-03-29 21:49:15 -0600

    convert object to array

--- script.js
+++ script.js
@@ -12,6 +12,29 @@ function mathOperation (num1, operator, num2) {
     if ( operator == '/' ); // Assign the operator
     return num1 / num2;
 }
+
 console.log(calculator(3, '*', 6));
 console.log(mathOperation(6, '/', 13));
 
+/**
+ * Assignment 2
+ * Write a function that converts and object into an array, where each element represents a key-value pair.
+ */
+
+// Declare the object value (JSON)
+
+let obj = {
+    'blue':    7,
+    'red':     6,
+    'green':   5,
+    'yellow':  4,
+    'violet':  3,
+    'crimson': 2,
+    'orange':  1
+}
+// selecting the object properties (reference path of the codelines below was gotten from stack overflow)
+let toArray = Object.keys(obj).map(function(key) { // define the result variable and set the iteration.
+    return [key, obj[key]] // converts the number to an array
+});
+
+console.log(toArray);
