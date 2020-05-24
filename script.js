4237058b8f267ac89cea0af9ef409112e512216d
leye20 <leyeagbo@gmail.com>
2020-05-23 18:13:19 -0600

    loop examples

--- script.js
+++ script.js
@@ -39,3 +39,22 @@ let toArray = Object.keys(obj).map(function(key) { // define the result variable
 });
 
 console.log(toArray);
+
+/**
+ * Code along on from lynda.com
+ * Trying out some exercises.
+ */
+
+ var i = 567;
+ var reps = 0;
+
+ if ( i <= 567 ) 
+ {
+    do {
+        ++reps;
+        console.log( reps + " reps gives us" + i );
+        i*=2.1;
+    } while ( i < 567 );
+} else {
+        console.log( "i is bigger than 567." );
+    }
